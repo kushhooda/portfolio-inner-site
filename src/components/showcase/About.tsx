90fb59cf8ffc8993d1e181c8ecd58c4962b05c65
kushhooda <kush9hooda@gmail.com>
2026-03-18 09:27:09 +0300

    Update About page with Kush's bio

--- src/components/showcase/About.tsx
+++ src/components/showcase/About.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import me from '../../assets/pictures/workingAtComputer.jpg';
-import meNow from '../../assets/pictures/currentme.jpg';
 import { Link } from 'react-router-dom';
 import ResumeDownload from './ResumeDownload';
 
@@ -8,17 +6,16 @@ export interface AboutProps {}
 
 const About: React.FC<AboutProps> = (props) => {
     return (
-        // add on resize listener
         <div className="site-page-content">
-            {/* <img src={me} style={styles.topImage} alt="" /> */}
             <h1 style={{ marginLeft: -16 }}>Welcome</h1>
-            <h3>I'm Henry Heffernan</h3>
+            <h3>I'm Kush Hooda</h3>
             <br />
             <div className="text-block">
                 <p>
-                    I'm a software engineer currently working at Vercel! In May
-                    of 2022 I graduated from Rensselaer Polytechnic Institute
-                    with my BS in Computer Science.
+                    I'm a passionate student, developer, and builder who loves
+                    turning ideas into real digital projects. I enjoy creating
+                    websites, apps, and game concepts that are not only
+                    functional but also creative and impactful.
                 </p>
                 <br />
                 <p>
@@ -27,8 +24,8 @@ const About: React.FC<AboutProps> = (props) => {
                     building it. If you have any questions or comments, feel
                     free to contact me using{' '}
                     <Link to="/contact">this form</Link> or shoot me an email at{' '}
-                    <a href="mailto:henryheffernan@gmail.com">
-                        henryheffernan@gmail.com
+                    <a href="mailto:kush9hooda@gmail.com">
+                        kush9hooda@gmail.com
                     </a>
                 </p>
             </div>
@@ -37,55 +34,19 @@ const About: React.FC<AboutProps> = (props) => {
                 <h3>About Me</h3>
                 <br />
                 <p>
-                    From a young age, I have had a curiosity about how things
-                    worked. This naturally led me to become absolutely obsessed
-                    with Lego and I fell in love with building things. In
-                    elementary school, I joined the Lego Robotics team at my
-                    local middle school, which was my first real exposure to
-                    programming. In 2008, my family and I moved across the
-                    country from California to New York, where I attended middle
-                    school, high school, and college.
-                </p>
-                <br />
-                <div className="captioned-image">
-                    <img src={me} style={styles.image} alt="" />
-                    <p>
-                        <sub>
-                            <b>Figure 1:</b> A real photo of me developing this
-                            website :)
-                        </sub>
-                    </p>
-                </div>
-
-                <p>
-                    I started programming more seriously in high school,
-                    initially learning how to scrape and interact with websites.
-                    I went on to do a ton of passion projects, many of them with
-                    one of my closest friends,{' '}
-                    <a
-                        rel="noreferrer"
-                        target="_blank"
-                        href="https://www.linkedin.com/in/scott-bass-189a7919b/"
-                    >
-                        Scott Bass
-                    </a>
-                    . We worked on many projects together, including chat bots,
-                    multiple game projects, apps, and more. One of these
-                    projects is viewable on my{' '}
-                    <Link to="/projects/software">Software Projects</Link> page.
+                    From portfolio sites to productivity tools and development
+                    projects, I'm always exploring new ways to build, improve,
+                    and innovate. I'm deeply interested in technology, design,
+                    entrepreneurship, and problem-solving.
                 </p>
                 <br />
                 <p>
-                    In 2017, I got accepted into Rennselear Polytechnic
-                    Institute to study Computer Science. It was my first choice
-                    and I was absolutely ecstatic to be going to such a great
-                    university. At the end of my sophomore year, I got an
-                    internship working for the startup Hover, primarily focusing
-                    on frontend work. I continued to work at Hover on and off
-                    for about a year and a half, until the start of my senior
-                    year when I decided to focus on other opportunities.
+                    I like learning by doing — whether that means developing a
+                    new project, refining user experience, or experimenting with
+                    fresh ideas that can grow into something meaningful. My goal
+                    is to keep building skills, creating valuable products, and
+                    turning ambition into reality.
                 </p>
-                <br />
                 <br />
                 <div style={{}}>
                     <div
@@ -96,50 +57,32 @@ const About: React.FC<AboutProps> = (props) => {
                             flexDirection: 'column',
                         }}
                     >
-                        <h3>My Hobbies</h3>
+                        <h3>My Interests</h3>
                         <br />
                         <p>
-                            Beyond software, I have a lot of hobbies that I
-                            enjoy doing in my free time. The more tangible
-                            hobbies I have are{' '}
-                            <Link to="/projects/music">Music Production</Link>{' '}
-                            and creating{' '}
-                            <Link to="/projects/art">Digital Art</Link>. You can
-                            read more about each of these on their respective
-                            pages under my projects tab. Some other hobbies I
-                            enjoy are working out, cooking, and (unsurprisingly)
-                            playing video games.
-                        </p>
-                        <br />
-                        <p>
-                            In college, I was an active member in the fraternity
-                            Sigma Alpha Epsilon and held multiple positions in
-                            the chapter. I met a lot of amazing people through
-                            my fraternity and thoroughly enjoyed the community.
-                        </p>
-                    </div>
-                    <div style={styles.verticalImage}>
-                        <img src={meNow} style={styles.image} alt="" />
-                        <p>
-                            <sub>
-                                <b>Figure 2:</b> Me, April 2022
-                            </sub>
+                            Beyond software, I have a wide range of interests.
+                            I'm passionate about{' '}
+                            <Link to="/projects/software">Software Projects</Link>{' '}
+                            and creating things that make an impact. You can
+                            read more about my work under the projects tab.
+                            Some other things I enjoy are working out, gaming,
+                            and staying on top of the latest in tech and
+                            entrepreneurship.
                         </p>
                     </div>
                 </div>
                 <br />
                 <br />
                 <p>
-                    Thanks for reading about me! I hope that you enjoy exploring
-                    the rest of my portfolio website and everything it has to
-                    offer. If you find the easter egg make sure to let me know
-                    on twitter{' '}
+                    Thanks for reading about me! I hope you enjoy exploring the
+                    rest of my portfolio. If you find the easter egg make sure
+                    to let me know on Instagram{' '}
                     <a
                         rel="noreferrer"
                         target="_blank"
-                        href="https://twitter.com/henryheffernan"
+                        href="https://www.instagram.com/whykushh/"
                     >
-                        @henryheffernan
+                        @whykushh
                     </a>{' '}
                     Good luck and have fun!
                 </p>
@@ -149,8 +92,8 @@ const About: React.FC<AboutProps> = (props) => {
                     them. You can reach me through the{' '}
                     <Link to="/contact">contact page</Link> or shoot me an email
                     at{' '}
-                    <a href="mailto:henryheffernan@gmail.com">
-                        henryheffernan@gmail.com
+                    <a href="mailto:kush9hooda@gmail.com">
+                        kush9hooda@gmail.com
                     </a>
                 </p>
             </div>
@@ -174,12 +117,9 @@ const styles: StyleSheetCSS = {
     },
     verticalImage: {
         alignSelf: 'center',
-        // width: '80%',
         marginLeft: 32,
         flex: 0.8,
-
         alignItems: 'center',
-        // marginBottom: 32,
         textAlign: 'center',
         flexDirection: 'column',
     },
